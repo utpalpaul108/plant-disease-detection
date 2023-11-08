3d4977081dfa73898cbceba58c0361b1dea35c77
Utpal Paul <utpalseu@gmail.com>
2023-11-09 00:01:56 +0600

    Data Ingestion Module Completed on Notebook

--- template.py
+++ template.py
@@ -16,7 +16,7 @@ list_of_files = [
     f'src/{project_name}/pipeline/__init__.py',
     f'src/{project_name}/entity/__init__.py',
     f'src/{project_name}/constants/__init__.py',
-    f'notebook/trials.ipynb',
+    f'notebooks/trials.ipynb',
     'config/config.yaml',
     'params.yaml',
     'setup.py',
